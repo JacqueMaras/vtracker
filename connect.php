4d5dfe1780ac6ddde0f5f8a627eaa322b2717fc6
JacquesMarais <marais.jacques11@gmail.com>
2014-02-08 09:24:53 +0200

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
 	include("config.php");
 	$mysqli = new mysqli($config['db_server'], $config['db_username'], $config['db_password'], $config['db_name']);
+	date_default_timezone_set($config['timezone']);
