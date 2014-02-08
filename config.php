7fec4e2d1333851f42d67a68a0012a749f4ce073
JacquesMarais <marais.jacques11@gmail.com>
2014-02-08 08:39:56 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// The database server: 
+$config['db_server'] = "localhost"; //normally "127.0.0.1" or "localhost"
+
+//Your database username:
+$config['db_username'] = "root"; //normally "root"
+
+//Your database password:
+$config['db_password'] = "";
+
+//Your database name:
+$config['db_name'] = "vtracker"; //if you don't have one please create one
